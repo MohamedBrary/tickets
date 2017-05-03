f252880bfca4097b303bcf3127a122edd2728cc9
Mohamed Brary <mohamedbrary@gmail.com>
2017-05-03 12:27:26 +0200

    Enhance layout and home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
 	def allowed_user_types
 		User.subclasses.collect{|type| type.to_s if type != Customer}.compact
 	end
+
+	def user_name
+		current_user.present? ? current_user.name : 'Guest'
+	end
 end
